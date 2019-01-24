31dffc18f883697e2d6418fad5e1a524f6d1f369
Haixiang <haixiang6123@gmail.com>
2019-01-24 01:17:09 -0800

    Add author info

--- src/components/DocPost.vue
+++ src/components/DocPost.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="doc-post">
+        <div class="author">
+            <img class="add-button" src="../assets/images/add-friend.png" alt="Add">
+            <span class="author-info-wrapper">
+                <img class="avatar" src="../assets/images/avatar.png" alt="Avatar">
+                <span class="name">HAIXIANG</span>
+            </span>
+        </div>
         <section v-for="section in doc.sections">
             <h1 v-if="section.h1">{{section.h1}}</h1>
             <h2 v-if="section.h2">{{section.h2}}</h2>
@@ -13,7 +20,7 @@
         name: "DocPost",
         props: {
             doc: {
-                type: Array,
+                type: Object,
                 required: true
             }
         }
@@ -22,12 +29,54 @@
 
 <style scoped lang="scss">
 .doc-post {
+    display: flex;
+    position: relative;
     padding: 10px 20px;
     margin: 1vh 3vw 1vh 0;
     height: 98vh;
     overflow: auto;
     background: rgba(245, 237, 221, 0.95);
     color: #2f3e4e;
+
+    .author {
+        display: flex;
+        position: absolute;
+        top: 0;
+        right: 0;
+
+        .add-button {
+            height: 25px;
+            width: 25px;
+            margin-right: 5px;
+            transition: all .3s;
+            &:hover {
+                box-shadow: 0px 0px 3px 3px #fff;
+            }
+            &:active {
+                transform: scale(0.9);
+            }
+        }
+
+        &-info-wrapper {
+            display: flex;
+            align-items: center;
+            height: 100%;
+            color: white;
+            background: rgba(0, 0, 0, 0.5);
+            border-left: 5px solid #b1fb23;
+            .name {
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding: 0 50px 0 10px;
+            }
+            .avatar {
+                height: 25px;
+                width: 25px;
+            }
+        }
+    }
     
     section {
         h1, h2 {
