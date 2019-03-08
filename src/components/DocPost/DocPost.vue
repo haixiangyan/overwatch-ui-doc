c7017da98c5993352ddee52200495e97e7f8c8b9
Haixiang <haixiang6123@gmail.com>
2019-03-08 13:06:58 -0800

    Update personal webpage

--- src/components/DocPost/DocPost.vue
+++ src/components/DocPost/DocPost.vue
@@ -45,7 +45,7 @@
         },
         methods: {
             jumpToGithub() {
-                window.location.href = 'https://github.com/Haixiang6123'
+                window.location.href = 'https://haixiang6123.github.io/blog/#/'
             },
             highlight() {
                 let blocks = this.$refs.markdownDoc.querySelectorAll('pre code')
